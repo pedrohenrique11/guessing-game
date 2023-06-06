032fc683e7dea776f6c765177fdbbd1af405f04b
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2023-06-06 18:23:01 +0000

    refactor main.js

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-const handleNumber = Math.round(Math.random() * 10);
-let attempts = 1;
+const fistScreen = document.querySelector(".screen1")
+const secondScreen =document.querySelector(".screen2")
 
-function handleClick(event) {
-    event.preventDefault()
+const handleNumber = Math.round(Math.random() * 10); // Número aleatorio de 1 a 10
+
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let attempts = 1; // Tentativas
+
+
+function handleTryClick(e) {
+    e.preventDefault();
     
     const inputNumber = document.querySelector("#inputNumber");
-
-    if(Number(inputNumber) == handleNumber) {
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
-        console.log("acertou cabeção!!!")
-    }
-    else console.log("errou")
-
-    attempts++
-}
     
-console.log(inputNumber)
+    if(Number(inputNumber.value) == handleNumber) {
+        fistScreen.classList.add("hide") // adicionando classe .hide
+        secondScreen.classList.remove("hide") // removendo a classe .hide
+        
+        document.querySelector("h2").innerText = `Acertou em ${attempts} tentativas!`
+    }
+    inputNumber.value = ""
+    attempts++ // Acresenta uma tentativa ao número de tentativas
+}
+function resetClick() {
+    secondScreen.classList.add("hide") // adicionando classe .hide
+    fistScreen.classList.remove("hide") // removendo a classe .hide
+
+    attempts = 1
+}
+
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', resetClick)
